bb207aaacde701d1bd6a9cf87a9d603b8998b27f
Jan Tomsic <jan.tomsic@datacarrot.com>
2024-11-08 10:48:24 +0100

    run

--- main.tf
+++ main.tf
@@ -1,11 +1,10 @@
 locals {
-  project = var.project_id
   region = "europe-west3"
   zone = "europe-west3-a"
 }
 
 provider "google" {
-  project = local.project
+  project = var.project_id
   region  = local.region
   zone    = local.zone
 }
@@ -29,14 +28,21 @@ resource "google_artifact_registry_repository" "my_repo" {
 
 # https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/cloud_run_v2_service
 resource "google_cloud_run_v2_service" "cts" {
-  name = "cts"
-  location = local.region
-  
+  name                = "cts"
+  location            = local.region
+  deletion_protection = false
+  ingress             = "INGRESS_TRAFFIC_ALL"
+
   template {
-    spec {
-      containers {
-        image = "${var.region}-docker.pkg.dev"
+    containers {
+      image = "${local.region}-docker.pkg.dev/${var.project_id}/${google_artifact_registry_repository.my_repo.repository_id}/cts:${var.app_version}"
+      resources {
+        limits = {
+          cpu    = "1"
+          memory = "128Mi"
+        }
       }
     }
   }
 }
+
